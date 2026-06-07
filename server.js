3adcfc31b5cca02dd28b6dc6370ce7b4096dfe2e
amanda12-create <amandarena923@gmail.com>
2026-06-07 23:31:04 +0700

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const MODEL_MAPPING = {
   'gpt-4': 'qwen/qwen3-coder-480b-a35b-instruct',
   'gpt-4-turbo': 'minimaxai/minimax-m2.7',
   'gpt-4o': 'deepseek-ai/deepseek-v4-pro',
-  'claude-3-opus': 'z-ai/glm4.7',
+  'claude-3-opus': 'stepfun-ai/step-3.7-flash',
   'claude-3-sonnet': 'z-ai/glm-5.1',
   'gemini-pro': 'moonshotai/kimi-k2.6'
 };
